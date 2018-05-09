7e45d8bcb2a0de0c16483cacaa02ff8749fc0f7d
minxindi <minxindi9@163.com>
2018-05-09 23:57:26 +0800

    第一次提交

---
+++ Multi_001/src/com/bjsxt/base/sync005/SyncException.java
@@ -0,0 +1,40 @@
+package com.bjsxt.base.sync005;
+/**
+ * synchronized异常
+ * @author alienware
+ *
+ */
+public class SyncException {
+
+	private int i = 0;
+	public synchronized void operation(){
+		while(true){
+			try {
+				i++;
+				Thread.sleep(100);
+				System.out.println(Thread.currentThread().getName() + " , i = " + i);
+				if(i == 10){
+					Integer.parseInt("a");
+					//throw new RuntimeException();
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				//continue;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		final SyncException se = new SyncException();
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				se.operation();
+			}
+		},"t1");
+		t1.start();
+	}
+	
+	
+}
